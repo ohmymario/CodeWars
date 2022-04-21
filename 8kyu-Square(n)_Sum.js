aec183e59b5955f2ba1389c0b54c5682af23ea2a
Mario <mariob360@gmail.com>
2022-04-21 16:20:10 -0700

    added 8kyu - Square(n) Sum

---
+++ 8kyu-Square(n)_Sum.js
@@ -0,0 +1,7 @@
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+function squareSum(numbers) {
+  const squared = numbers.map((item) => item ** 2);
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  return squared.reduce(reducer, 0);
+}
